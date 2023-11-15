c4584c5915b07b6e9ce8145fcd38c588aa1165e5
Grace Olawumi <gracewumi26@gmail.com>
2023-11-15 12:52:21 +0100

    first commit

---
+++ Backend API/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetUserNames()
+        {
+            string[] studentNames = new string[] { "Grace", "Joan", "Dara" };
+            return Ok(studentNames);
+        }
+
+    }
+}
